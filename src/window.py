a71c67f2d47af9b8d98414ebe050346677e0100f
Antoine Venturelli <ant.venturelli06@gmail.com>
2021-11-25 14:30:05 +0100

    #2 Panel pour visualiser toute les images
    Co-Authored-By: FissoreD <79260701+FissoreD@users.noreply.github.com>

--- src/window.py
+++ src/window.py
@@ -5,15 +5,27 @@
         ask for images to image_reader library
 """
 
-import tkinter as tk
-from typing import List
-import images as img
+from tkinter import Scrollbar, ttk
 from PIL import ImageTk, Image
-from tkinter import ttk
+import images as img
+from typing import Collection, List
+from tkinter.constants import HORIZONTAL
+import tkinter as tk
+import tags
+from scrollableframe import ScrollableFrame
 
 
 def get_selected_images(list_images: List[img.Img]):
     return [i for i in list_images if i.is_selected]
+
+
+def createLabel(im, frm):
+
+    photo = ImageTk.PhotoImage(im.img)
+    imgLabel = ttk.Label(frm.scrollable_frame, image=photo)
+    imgLabel.image = photo
+    imgLabel.pack()
+    return imgLabel
 
 
 class ImageMakeRect():
@@ -29,30 +41,47 @@ class ImageScrollBar:
     import tkinter as tk
 
     def __init__(self, list_img, window):
-
-        self.scrollbar = tk.Scrollbar(window, orient=tk.VERTICAL)
-        self.scrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
-
-        self.canvas = tk.Canvas(
-            self.scrollbar, bd=0, highlightthickness=0, yscrollcommand=self.scrollbar.set)
-        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
-        self.scrollbar.config(command=self.canvas.yview)
-
-        # reset the view
-        self.canvas.xview_moveto(0)
-        self.canvas.yview_moveto(0)
-
-        for img in list_img:
-            img.img.show()
-            self.canvas.create_image(0, 0, anchor=tk.NW, image=img.img)
+        self.frm = tk.Frame(window)
+        self.canvas = tk.Label(
+            self.frm, image=ImageTk.PhotoImage(list_img[0].img))
+        self.canvas.pack()
 
 
 def main(list_tag, list_img):
+    """
     print(123)
     root_window = tk.Tk()
     notebook = ttk.Notebook(root_window)
     # notebook.add(ImageMakeRect(
     #     notebook, list_img[0]).canvas, text="All images")
     #notebook.add(grep(notebook), text="Selected images")
-    scrollbar = ImageScrollBar(list_img, root_window)
+    # scrollbar = ImageScrollBar(list_img, root_window)
+    im = ImageTk.PhotoImage(list_img[0].img)
+    frame = tk.Frame(root_window)
+    label = tk.Label(frame, image=im)
+    label.image = im
     root_window.mainloop()
+    """
+
+    root = tk.Tk()
+    root.title('ImageAnnotator')
+    root.geometry('400x300')
+
+    frm = ScrollableFrame(root)
+
+    frm.grid(row=100, column=100)
+
+    for i in range(len(list_img)):
+        createLabel(list_img[i], frm)
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+
+    tag_list = tags.Tag(img.open_files())
+    img_list = tag_list.imgs
+
+    for i in img_list:
+        i.set_tag_list(tag_list)
+    main(tag_list, img_list)
