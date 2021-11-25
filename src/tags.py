e29eefdb30cb94db950a2ef0c0ae80db2817fb39
FissoreD <79260701+FissoreD@users.noreply.github.com>
2021-11-25 10:18:46 +0100

    Add starting files

---
+++ src/tags.py
@@ -0,0 +1,22 @@
+from typing import List
+import images
+
+
+class Tag(set):
+    def __init__(self, imgs: list) -> None:
+        self.imgs: List[images.Img] = imgs
+
+    def add(self, __object) -> None:
+        return super().add(__object)
+
+    def remove(self, __value):
+        super().remove(__value)
+        return self.image_remove_tag(__value)
+
+    def rename(self, old_value, value):
+        [img.update_tag(old_value, value) for img in self.imgs]
+        self.add(value)
+        super().remove(old_value)
+
+    def image_remove_tag(self, tag: str):
+        [img.remove_tag(tag) for img in self.imgs]
---
+++ src/window.py
@@ -0,0 +1,8 @@
+"""
+    This file aims to create the guy for the ImageAnnotator
+    It only contains the structure and :
+        ask for images to image_reader library
+"""
+
+import tkinter
+
