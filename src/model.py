b61ad193ccd13ec2353471e3611fa5ffcde070a2
Antoine Venturelli <ant.venturelli06@gmail.com>
2022-01-25 00:21:22 +0100

    Model 5 is the best

--- src/model.py
+++ src/model.py
@@ -4,8 +4,11 @@ from tensorflow.keras import layers
 from global_vars import *
 import tensorflow as tf
 import os
+
+
 def get_class_names():
     return os.listdir(crop_dir)
+
 
 def make_model():
     data_augmentation = keras.Sequential(
@@ -19,26 +22,26 @@ def make_model():
     class_names = get_class_names()
     num_classes = len(class_names)
 
-#Test model 7 
-    model  = keras.models.Sequential([
-        data_augmentation,
-        layers.Rescaling(1./255),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Dropout(0.2),
-        layers.Flatten(),
-        #layers.Dense(128, activation='relu'), py, 
-        layers.Dense(num_classes)
-    ])
+# Test model 7
+    # model  = keras.models.Sequential([
+    #     data_augmentation,
+    #     layers.Rescaling(1./255),
+    #     layers.Conv2D(16, 3, padding='same', activation='relu'),
+    #     layers.MaxPooling2D(),
+    #     layers.Conv2D(16, 3, padding='same', activation='relu'),
+    #     layers.MaxPooling2D(),
+    #     layers.Conv2D(32, 3, padding='same', activation='relu'),
+    #     layers.MaxPooling2D(),
+    #     layers.Conv2D(32, 3, padding='same', activation='relu'),
+    #     layers.MaxPooling2D(),
+    #     layers.Dropout(0.2),
+    #     layers.Flatten(),
+    #     #layers.Dense(128, activation='relu'), py,
+    #     layers.Dense(num_classes)
+    # ])
 
 
-# #Test model 6 
+# #Test model 6
 #     model  = keras.models.Sequential([
 #         data_augmentation,
 #         layers.Rescaling(1./255),
@@ -50,31 +53,31 @@ def make_model():
 #         layers.MaxPooling2D(),
 #         layers.Dropout(0.2),
 #         layers.Flatten(),
-#         #layers.Dense(128, activation='relu'), py, 
+#         #layers.Dense(128, activation='relu'), py,
 #         layers.Dense(num_classes)
 #     ])
 
 
-# #Test model 5 
-#     model  = keras.models.Sequential([
-#         data_augmentation,
-#         layers.Rescaling(1./255),
-#         layers.Conv2D(16, 3, padding='same', activation='relu'),
-#         layers.MaxPooling2D(),
-#         layers.Conv2D(16, 3, padding='same', activation='relu'),
-#         layers.MaxPooling2D(),
-#         #layers.Conv2D(64, 3, padding='same', activation='relu'),
-#         layers.MaxPooling2D(),
-#         layers.Dropout(0.2),
-#         layers.Flatten(),
-#         #layers.Dense(128, activation='relu'), py, 
-#         layers.Dense(num_classes)
-#     ])
+# #Test model 5
+    model = keras.models.Sequential([
+        data_augmentation,
+        layers.Rescaling(1./255),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        #layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        #layers.Dense(128, activation='relu'), py,
+        layers.Dense(num_classes)
+    ])
 
 # #Test model 4
 #     model = keras.models.Sequential([
 #         data_augmentation,
-#         layers.Rescaling(1./255), 
+#         layers.Rescaling(1./255),
 #         layers.Conv2D(16, 3, padding='same', activation='relu'),
 #         layers.MaxPooling2D(2),
 #         layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -84,19 +87,17 @@ def make_model():
 #         layers.Conv2D(128, 3, padding='same', activation='relu'),
 #         layers.MaxPooling2D(4),
 #         layers.Dropout(0.2),
-#         layers.BatchNormalization(), 
+#         layers.BatchNormalization(),
 #         layers.Flatten(),
 #         #layers.Dense(128, activation='relu'),py,
 #         layers.Dense(num_classes)
-        
+
 #     ])
 
-
-
-    #Test model 3
+    # Test model 3
     # model = keras.models.Sequential([
     #     data_augmentation,
-    #     layers.Rescaling(1./255), 
+    #     layers.Rescaling(1./255),
     #     layers.Conv2D(128, 3, padding='same', activation='relu'),
     #     layers.MaxPooling2D(4),
     #     layers.Conv2D(64, 3, padding='same', activation='relu'),
@@ -104,17 +105,17 @@ def make_model():
     #     #layers.Conv2D(64, 3, padding='same', activation='relu'),
     #     layers.MaxPooling2D(2),
     #     layers.Dropout(0.4),
-    #     layers.BatchNormalization(), 
+    #     layers.BatchNormalization(),
     #     layers.Flatten(),
     #     #layers.Dense(128, activation='relu'),py,
     #     layers.Dense(num_classes)
-        
+
     # ])
 
-    #Test model 2
+    # Test model 2
     # model = keras.models.Sequential([
     #     data_augmentation,
-    #     layers.Rescaling(1./255), 
+    #     layers.Rescaling(1./255),
     #     layers.Conv2D(128, 3, padding='same', activation='relu'),
     #     layers.MaxPooling2D(2),
     #     layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -122,18 +123,17 @@ def make_model():
     #     #layers.Conv2D(64, 3, padding='same', activation='relu'),
     #     layers.MaxPooling2D(),
     #     layers.Dropout(0.2),
-    #     layers.BatchNormalization(), 
+    #     layers.BatchNormalization(),
     #     layers.Flatten(),
     #     #layers.Dense(128, activation='relu'),py,
     #     layers.Dense(num_classes)
-        
-    # ])
- 
 
-    # Test model 1 - This model isn't good, the error is high  
+    # ])
+
+    # Test model 1 - This model isn't good, the error is high
     # model = keras.models.Sequential([
     #     data_augmentation,
-    #     layers.Rescaling(1./255), 
+    #     layers.Rescaling(1./255),
     #     layers.Conv2D(16, 3, padding='same', activation='relu'),
     #     layers.MaxPooling2D(2),
     #     layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -141,13 +141,12 @@ def make_model():
     #     layers.Conv2D(64, 3, padding='same', activation='relu'),
     #     layers.MaxPooling2D(),
     #     layers.Dropout(0.2),
-    #     layers.BatchNormalization(), 
+    #     layers.BatchNormalization(),
     #     layers.Flatten(),
     #     #layers.Dense(128, activation='relu'),py,
     #     layers.Dense(num_classes)
-        
+
     # ])
- 
 
     f = tf.keras.losses.SparseCategoricalCrossentropy
     model.compile(optimizer='adam',
